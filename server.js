2c16aee65f6e50d35a35498150f9fb61356a5154
Bezael Pérez <bezael_e@hotmail.com>
2017-11-11 09:37:05 +0100

    Update route about

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(middleware.logger);
 app.use(express.static(__dirname+'/public'));
 
 app.get('/about', middleware.requireAuthenticaion,(req, res)=>{
-	res.send('About us');
+	res.send('About us!');
 });
 
 app.listen(port,()=>{
